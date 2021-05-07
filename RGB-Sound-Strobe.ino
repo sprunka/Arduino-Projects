e5f3665bce469d0019e6d28b8c98c14f4192b487
Sean Prunka <sprunka@gmail.com>
2021-05-07 01:35:28 -0400

    Added some Button Stuff. And More CONSTANTS
    The button stuff isn't working right, so I yoinked it out via comments.

--- RGB-Sound-Strobe.ino
+++ RGB-Sound-Strobe.ino
@@ -1,11 +1,34 @@
 #define LEDstripBlue 9
 #define LEDstripGreen 10
 #define LEDstripRed 11
+#define LED_ON 0
+#define LED_OFF 255
+#define RED 0
+#define BLUE 1
+#define GREEN 2
+#define CYAN 3
+#define MAGENTA 4
+#define YELLOW 5
+#define BLACK 6
+#define WHITE 7
+
+// const int buttonPin = 2;
+const int soundSensorPin = 7;
+
+// int currentState = HIGH;
+// int previousState = LOW;
+// int reading;
+int soundState = 0;
+
+// unsigned long timer = 0;
+// unsigned long debounce = 200UL;
 
 void setup()
 {
-  Serial.begin(9600);
-  pinMode(7, INPUT); //SIG of the Parallax Sound Impact Sensor connected to Digital Pin 7
+  //Serial.begin(9600);
+  pinMode(soundSensorPin, INPUT); //SIG of the Parallax Sound Impact Sensor connected to Digital Pin 7
+  // initialize the pushbutton pin as an input:
+  // pinMode(buttonPin, INPUT);
   pinMode(LEDstripBlue, OUTPUT);
   pinMode(LEDstripGreen, OUTPUT);
   pinMode(LEDstripRed, OUTPUT);
@@ -15,49 +38,80 @@ void setup()
 void loop()
 {
   // This all feels so bass-ackwards, honestly. But it works the way I waned it to.
-  // If there is a sound, it will light a randomly selected combination of LEDs.
-  boolean soundstate = digitalRead(7);
+  // If there is a sound, it will light a (weighted) psuedo-random selected combination of LEDs.
+  soundState = digitalRead(soundSensorPin);
+//  int reading = digitalRead(buttonPin);
+//
+//  if (reading == LOW && previousState == HIGH && millis() - timer > debounce) {
+//  Serial.print("READING--");
+//  Serial.println(reading);
+//    if (currentState == HIGH) {
+//      Serial.println("SETTING CURRENT = LOW");
+//      currentState = LOW;
+//    } else {
+//      Serial.println("SETTING CURRENT = HIGH");
+//      currentState = HIGH;
+//    }
+//    timer = millis();
+//  }
+//  
+  int color = random(18);
 
-  int color = random(7);
-
-  if (soundstate == 0 && color == 0) {
-    analogWrite(LEDstripBlue, 255);
-    analogWrite(LEDstripGreen, 0);
-    analogWrite(LEDstripRed, 0);
-    delay(10);
-  } else if (soundstate == 0 && color == 1) {
-    analogWrite(LEDstripGreen, 255);
-    analogWrite(LEDstripBlue, 0);
-    analogWrite(LEDstripRed, 0);
-    delay(10);
-  } else if (soundstate == 0 && color == 2) {
-    analogWrite(LEDstripBlue, 0);
-    analogWrite(LEDstripGreen, 0);
-    analogWrite(LEDstripRed, 255);
-    delay(10);
-  } else if (soundstate == 0 && color == 3) {
-    analogWrite(LEDstripGreen, 0);
-    analogWrite(LEDstripRed, 255);
-    analogWrite(LEDstripBlue, 255);
-    delay(10);
-  } else if (soundstate == 0 && color == 4) {
-    analogWrite(LEDstripBlue, 0);
-    analogWrite(LEDstripRed, 255);
-    analogWrite(LEDstripGreen, 255);
-    delay(10);
-  } else if (soundstate == 0 && color == 5) {
-    analogWrite(LEDstripRed, 0);
-    analogWrite(LEDstripBlue, 255);
-    analogWrite(LEDstripGreen, 255);
-    delay(10);
-  } else if (soundstate == 0 && color == 6) {
-    analogWrite(LEDstripBlue, 255);
-    analogWrite(LEDstripGreen, 255);
-    analogWrite(LEDstripRed, 255);
-    delay(10);
+  if (soundState == 0)
+  {
+    if (color == GREEN) {
+      // GREEN
+      analogWrite(LEDstripGreen, LED_ON);
+      analogWrite(LEDstripBlue, LED_OFF);
+      analogWrite(LEDstripRed, LED_OFF);
+    } else if (color == RED || color == 8 || color == 11 || color == 13 || color == 15) {
+      //RED
+      analogWrite(LEDstripBlue, LED_OFF);
+      analogWrite(LEDstripGreen, LED_OFF);
+      analogWrite(LEDstripRed, LED_ON);
+      delay(10);
+    } else if (color == BLUE || color == 9 || color == 16) {
+      //BLUE
+      analogWrite(LEDstripGreen, LED_OFF);
+      analogWrite(LEDstripRed, LED_OFF);
+      analogWrite(LEDstripBlue, LED_ON);
+      delay(10);
+    } else if (color == YELLOW) {
+      //YELLOW
+      analogWrite(LEDstripBlue, LED_OFF);
+      analogWrite(LEDstripRed, LED_ON);
+      analogWrite(LEDstripGreen, LED_ON);
+      delay(10);
+    } else if (color == MAGENTA || color == 10 || color == 12 || color == 14 || color == 17) {
+      //MAGENTA
+      analogWrite(LEDstripRed, LED_ON);
+      analogWrite(LEDstripBlue, LED_ON);
+      analogWrite(LEDstripGreen, LED_OFF);
+      delay(10);
+    } else if (color == CYAN) {
+      //CYAN
+      analogWrite(LEDstripRed, LED_OFF);
+      analogWrite(LEDstripBlue, LED_ON);
+      analogWrite(LEDstripGreen, LED_ON);
+    } else if (color == WHITE || color == 18) {
+      //WHITE
+      analogWrite(LEDstripBlue, LED_ON);
+      analogWrite(LEDstripGreen, LED_ON);
+      analogWrite(LEDstripRed, LED_ON);
+    }
+    delay(25);
   } else {
-    analogWrite(LEDstripBlue, 255);
-    analogWrite(LEDstripGreen, 255);
-    analogWrite(LEDstripRed, 255);
+   // if (currentState == HIGH) {
+      // WHITE
+   //   analogWrite(LEDstripBlue, LED_ON);
+   //   analogWrite(LEDstripGreen, LED_ON);
+   //   analogWrite(LEDstripRed, LED_ON);
+   // } else {
+      //BLACK
+      analogWrite(LEDstripBlue, LED_OFF);
+      analogWrite(LEDstripGreen, LED_OFF);
+      analogWrite(LEDstripRed, LED_OFF);
+  //  }
+  //previousState = HIGH;
   }
 }
